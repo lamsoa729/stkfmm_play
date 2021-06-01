16942b6bb601c9e97e201f2e933f350533fc0f2b
Adam Lamson <lamsoa729@gmail.com>
2021-06-01 11:53:05 -0400

    Added executable to cmake

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @author      : alamson (alamson@$HOSTNAME)
+ * @file        : main
+ * @created     : Tuesday Jun 01, 2021 11:32:23 EDT
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
